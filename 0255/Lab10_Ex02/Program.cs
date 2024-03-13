773b4b347289ce65c581ee3b7c6cd1ef827c39b3
Sorawit255 <65030255@kmitl.ac.th>
2024-03-13 19:33:15 +0700

    2

---
+++ 0255/Lab10_Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    public Shape()
+    {
+        System.Console.WriteLine("This is some shape");
+    }
+}
+class Circle :Shape
+{
+    public Circle()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+class Rectangle :Shape
+{
+    public Rectangle()
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+class Triangle :Shape
+{
+   public Triangle()
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
