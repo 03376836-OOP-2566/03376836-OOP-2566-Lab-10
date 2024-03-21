99dae5c3b9fb5d7ca992d1ec5f41089b8a63898c
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 04:00:33 +0700

    03376836-OOP-2566-Lab-10

---
+++ 0289/Lab10_Ex01/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+var shape = new Shape();
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    public Shape()
+    {
+        System.Console.WriteLine("This is some shape");
+    }
+}
+class Circle
+{
+    public Circle()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+class Rectangle
+{
+    public Rectangle()
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+class Triangle
+{
+   public Triangle()
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
---
+++ 0289/Lab10_Ex03/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+    public Shape()
+    {
+        System.Console.WriteLine("This is some shape with unknown side");
+    }
+    public Shape(int NumOfSide)
+    {
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides" );
+    }
+}
+class Circle :Shape
+{
+    public Circle():base()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+class Rectangle :Shape
+{
+    public Rectangle(): base(4)
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+class Triangle :Shape
+{
+   public Triangle() : base(3)
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
