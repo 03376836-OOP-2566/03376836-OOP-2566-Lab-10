6951bceb76f8c1a8b0f2d80302bcb8b4245ae13b
likunzz <65030028@kmitl.ac.th>
2024-03-22 11:21:19 +0700

    ๅ

---
+++ 0028/Lab10_Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿var shape = new Shape();
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    public Shape()
+    {
+        System.Console.WriteLine("This is some shape");
+    }
+}
+class Circle
+{
+    public Circle()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+class Rectangle
+{
+    public Rectangle()
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+class Triangle
+{
+   public Triangle()
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
