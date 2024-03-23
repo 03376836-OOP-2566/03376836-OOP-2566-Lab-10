4a2a805597879a79e5ca61aebf1683f1d03b1183
KanyakornPuengmon <144195697+KanyakornPuengmon@users.noreply.github.com>
2024-03-23 20:42:45 +0700

    Add files via upload

---
+++ Lab10_Ex05/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    private int? NumOfSide;
+
+    private Shape()
+    {
+    }
+
+    public Shape(int NumOfSide) : this()
+    {
+        this.NumOfSide = NumOfSide;
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base(0)
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+    public Triangle() : base(3) // ส่งค่า 3 เพื่อระบุว่าเป็นสามเหลี่ยม (มี 3 ด้าน)
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
+
