4c50bb63d784b75c419d4c7e3f49bc8866371523
nitiphum <65030130@kmitl.ac.th>
2024-03-24 02:35:55 +0700

    สร้างex5

--- Lab10_Ex05/Program.cs
+++ Lab10_Ex05/Program.cs
@@ -5,18 +5,18 @@ var triangle = new Triangle();
 class Shape
 {
     private int? NumOfSide;
-    public Shape()
+    private Shape()
     {
-        System.Console.WriteLine("This is some shape with unknown side");
     }
-    public Shape(int NumOfSide)
+    public Shape(int NumOfSide) : this()
     {
+       this.NumOfSide = NumOfSide;
         System.Console.WriteLine($"This is some shape with {NumOfSide} sides" );
     }
 }
 class Circle :Shape
 {
-    public Circle():base()
+    public Circle():base(0)
     {
         System.Console.WriteLine("This is a circle");
     }
@@ -30,7 +30,7 @@ class Rectangle :Shape
 }
 class Triangle :Shape
 {
-   public Triangle() : base(3)
+   public Triangle() : base( ) // Hint: add number of size as parameters
     {
         System.Console.WriteLine("This is a triangle");
     }
