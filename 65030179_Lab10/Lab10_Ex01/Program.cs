43d5063f27fd66fc6953d4bdc57ae7158c8d3bd5
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-25 00:37:42 +0700

    commit Ex01

--- 65030179_Lab10/Lab10_Ex01/Program.cs
+++ 65030179_Lab10/Lab10_Ex01/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var shape = new Shape();
+var circle = new Circle();
+var rectangle = new Rectangle();
+var triangle = new Triangle();
+
+class Shape
+{
+    public Shape()
+    {
+        System.Console.WriteLine("This is some shape");
+    }
+}
+class Circle
+{
+    public Circle()
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+class Rectangle
+{
+    public Rectangle()
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+class Triangle
+{
+   public Triangle()
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
